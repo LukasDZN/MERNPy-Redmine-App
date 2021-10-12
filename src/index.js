4d46b24e2ba054b13ee1f72e0613c4ffe50571d3
LukasDZN <lukasdzn1@gmail.com>
2021-10-12 21:58:45 +0300

    added button, non working yet

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Source: https://mui.com/getting-started/usage/
+// Installation: npm install @mui/material @emotion/react @emotion/styled
+import Button from '@mui/material/Button';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// To do:
+// 1. User clicks a button to either get data for the first time or refresh redmine task data.
+// ------ Currently need for a button to make a request 
+// 2. The button shows that it's loading as it waits for a response.
+// 3. The button shows 'Completed' and turns green.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Either get data for the first time or refresh redmine task data.
+const getAllSupportTasks = async () => {
+  await fetch('http://localhost:5000/getAllSupportTasks', {
+    method: 'GET'
+  })
+};
+
+function App() {
+  return <Button variant="contained">Refresh Redmine task data</Button>;
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
+
