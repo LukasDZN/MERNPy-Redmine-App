6f68bb5db72b2e9167ef21b20f5a52801ec9d5a7
LukasDZN <lukasdzn1@gmail.com>
2021-10-15 23:04:09 +0300

    working styled buttons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-// Source: https://mui.com/getting-started/usage/
-// Installation: npm install @mui/material @emotion/react @emotion/styled
-import Button from '@mui/material/Button';
+import '../src/index.css'; // need to import css, because in React it doesn't go to index.html
+
 
 // To do:
 // 1. User clicks a button to either get data for the first time or refresh redmine task data.
@@ -10,16 +9,40 @@ import Button from '@mui/material/Button';
 // 2. The button shows that it's loading as it waits for a response.
 // 3. The button shows 'Completed' and turns green.
 
-// Either get data for the first time or refresh redmine task data.
-const getAllSupportTasks = async () => {
-  await fetch('http://localhost:5000/getAllSupportTasks', {
+
+
+// Update the announcement sheet (anTaskSheetUpdate)
+const anTaskSheetUpdate = async () => {
+  let anTaskSheetUpdateReponse = await fetch('http://localhost:5000/anTaskSheetUpdate', {
     method: 'GET'
-  })
+  });
+  return null
 };
 
+
+// Either get data for the first time or refresh redmine task data.
+const getAllSupportTasks = async () => {
+  let getAllSupportTasksReponse = await fetch('http://localhost:5000/getAllSupportTasks', {
+    method: 'GET'
+  });
+  return null
+};
+
+// Either get data for the first time or refresh redmine task data.
+// const createRfc = async () => {
+//   let createRfcReponse = await fetch('http://localhost:5000/', {
+//     method: 'GET'
+//   });
+// };
+
 function App() {
-  return <Button variant="contained">Refresh Redmine task data</Button>;
-}
+  return (
+    <div>
+      <button onClick={getAllSupportTasks} className="button-28">Refresh Redmine task data</button>
+      <button onClick={anTaskSheetUpdate} className="button-28">Update the announcement sheet</button>
+    </div>
+  )
+};
 
 ReactDOM.render(<App />, document.querySelector('#app'));
 
