4c4cd2e301a1fd354ed98de47f879c012ef624d0
ld <ld@tribepayments.com>
2022-01-01 16:23:30 +0200

    backend domain set to variable depending on mode - prod or dev

--- src/components/config.js
+++ src/components/config.js
@@ -4,7 +4,17 @@
 //http://192.168.1.66:5000
 export default function config() {
 
-    return {
-        "BACKEND_DOMAIN": `http://localhost:5000`
+    // Set the backend domain (change the var below when developing locally)
+    let mode = 'production'; 
+
+    if (mode === 'production') {
+        return {
+            'BACKEND_DOMAIN': 'http://tribeserver.ddns.net'
+        }
+    } 
+    else if (mode === 'development') {
+        return {
+            'BACKEND_DOMAIN': 'http://localhost:5000'
+        }
     }
 }
